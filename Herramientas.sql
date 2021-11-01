32fb553d840921f30234258cb323d987e84c1e0a
Lucas Ezequiel Barriento <62809823+Lucas-Barriento@users.noreply.github.com>
2021-11-01 02:02:34 -0300

    Add files via upload
    Se agrega la base de datos de herramientas y los datos

---
+++ Herramientas.sql
@@ -0,0 +1,51 @@
+CREATE DATABASE Herramientas
+go
+use Herramientas
+go
+create table Marcas(
+id bigint not null primary key identity(1,1),
+nombre varchar(50) not null,
+)
+go
+create table Categorias(
+id bigint not null primary key identity(1,1),
+nombre varchar(50) not null,
+)
+
+go
+create table Usuario(
+dni bigint not null primary key,
+nombre varchar(50) not null,
+apellido varchar(50) not null,
+nacimiento date not null,
+mail varchar(50) not null,
+nombreusuario varchar(50) not null,
+contraseña varchar(50) not null,
+telefono varchar(50) not null,
+)
+go
+create table Articulo(
+id bigint not null primary key identity(1,1),
+nombre varchar(50) not null,
+idMarca bigint foreign key references Marcas(id) not null,
+idCategoria bigint foreign key references Categorias(id),
+descripcion varchar(50) not null,
+precio money not null,
+peso_kg decimal null,
+largo_cm decimal null,
+imagenURL varchar(50) not null,
+stock bigint not null,
+Estado bit null,
+)
+go
+create table Carrito(
+id bigint not null primary key identity(1,1),
+idusuario bigint not null references Usuario(dni), 
+precioTotal money not null,
+)
+go
+create table ArticuloCarrito(
+id bigint not null foreign key references Carrito(id),
+idarticulo bigint not null references Articulo(id),
+cantidad bigint not null,
+)
