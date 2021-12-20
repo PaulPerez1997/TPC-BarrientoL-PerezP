7d83dc39e09c7e3332b05c2d5ba4b5cbffc02811
PaulPerez19 <paulalbertoperez1997@gmail.com>
2021-12-20 19:40:49 -0300

    Se Valida Stocks, de acuerdo a las compras

--- TPC-BarrientoL-PerezP/Comprar.aspx.cs
+++ TPC-BarrientoL-PerezP/Comprar.aspx.cs
@@ -34,69 +34,96 @@ namespace TPC_BarrientoL_PerezP
 
         protected void BtnComprar_Click(object sender, EventArgs e)
         {
-         
 
+
+
+         
             int cantidad = int.Parse(TBCantidad.Text);
-            decimal PrecioTotal = cantidad * ArticuloEnVenta.Precio;
+
            
-            if ( cantidad > ArticuloEnVenta.Stock)
+            //if (cantidad == stock)
+            //{
+
+            //}
+            //else
+            //{
+
+
+            //}
+
+
+                decimal PrecioTotal = cantidad * ArticuloEnVenta.Precio;
+
+            if (cantidad > ArticuloEnVenta.Stock)
             {
                 Session.Add("error", "Error al Comprar! La Cantidad Supera al Stock del Articulo ");
                 Response.Redirect("error.aspx", false);
             }
-
-
-            Persona Comprador = (Persona)Session["Usuario"];
-
-            CompraRealizada compra = new CompraRealizada();
-            compra.IDArticulo = ArticuloEnVenta.ID;
-            compra.DniUsuario = Comprador.dni;
-            compra.Fecha = DateTime.Now;
-            CompraRealizadaDatos compranueva = new CompraRealizadaDatos();
-
-            
-
-        
-            
-            DetalleCompraDatos detalleDatos = new DetalleCompraDatos();
-            DetalleCompras ticket = new DetalleCompras();
-          
-
-           
-
-           
-            if (compranueva.Agregar(compra))
+            else
             {
-                int UltimoIdCompraRealizada = compranueva.UltimoIDCompraRealizadaxdni(Comprador.dni);
+                ArticuloDatos stockdatos = new ArticuloDatos();
+                int stock = (int)ArticuloEnVenta.Stock;
+                int nuevostock = stock - cantidad;
 
-                ticket.IDCompraRealizada = UltimoIdCompraRealizada;
-                ticket.IDArticulo = ArticuloEnVenta.ID;
-                ticket.NombreArticulo = ArticuloEnVenta.Nombre;
-                ticket.PrecioTotal = PrecioTotal;
-                ticket.imagenURL = ArticuloEnVenta.ImagenURL;
-                ticket.DNIComprador = Comprador.dni;
-                ticket.NombreUsuarioComprador = Comprador.nombreusuario;
-                ticket.NombreComprador = Comprador.nombres;
-                ticket.ApellidoComprador = Comprador.apellido;
-                ticket.TelefonoComprador = Comprador.telefono;
-                ticket.MailComprador = Comprador.mail;
-                ticket.DireccionAEnviar = TBDireccion.Text;
-                ticket.DNIVendedor = user.dni;
-                ticket.NombreUsuarioVendedor = user.nombreusuario;
-                ticket.NombreVendedor = user.nombres;
-                ticket.ApellidoVendedor = user.apellido;
-                ticket.TelefonoVendedor = user.telefono;
-                ticket.MailVendedor = user.mail;
-
-                if( detalleDatos.agregar(ticket) == true)
+                if ( cantidad == ArticuloEnVenta.Stock)
                 {
+                    stockdatos.ActualizarStock(ArticuloEnVenta.ID, nuevostock);
+                    stockdatos.Eliminar(ArticuloEnVenta.ID);
 
-                    Session.Add("exito", "Enhorabuena Tu Compra se realizo con exito! ");
-                    Response.Redirect("ComprasRealizadas.aspx", false);
                 }
-            
+                else
+                {
+               
 
-                
+                    stockdatos.ActualizarStock(ArticuloEnVenta.ID,nuevostock);
+
+                }
+
+
+
+                Persona Comprador = (Persona)Session["Usuario"];
+
+                CompraRealizada compra = new CompraRealizada();
+                compra.IDArticulo = ArticuloEnVenta.ID;
+                compra.DniUsuario = Comprador.dni;
+                compra.Fecha = DateTime.Now;
+                CompraRealizadaDatos compranueva = new CompraRealizadaDatos();
+
+                DetalleCompraDatos detalleDatos = new DetalleCompraDatos();
+                DetalleCompras ticket = new DetalleCompras();
+
+                if (compranueva.Agregar(compra))
+                {
+                    int UltimoIdCompraRealizada = compranueva.UltimoIDCompraRealizadaxdni(Comprador.dni);
+
+                    ticket.IDCompraRealizada = UltimoIdCompraRealizada;
+                    ticket.IDArticulo = ArticuloEnVenta.ID;
+                    ticket.NombreArticulo = ArticuloEnVenta.Nombre;
+                    ticket.PrecioTotal = PrecioTotal;
+                    ticket.imagenURL = ArticuloEnVenta.ImagenURL;
+                    ticket.DNIComprador = Comprador.dni;
+                    ticket.NombreUsuarioComprador = Comprador.nombreusuario;
+                    ticket.NombreComprador = Comprador.nombres;
+                    ticket.ApellidoComprador = Comprador.apellido;
+                    ticket.TelefonoComprador = Comprador.telefono;
+                    ticket.MailComprador = Comprador.mail;
+                    ticket.DireccionAEnviar = TBDireccion.Text;
+                    ticket.DNIVendedor = user.dni;
+                    ticket.NombreUsuarioVendedor = user.nombreusuario;
+                    ticket.NombreVendedor = user.nombres;
+                    ticket.ApellidoVendedor = user.apellido;
+                    ticket.TelefonoVendedor = user.telefono;
+                    ticket.MailVendedor = user.mail;
+
+                    if (detalleDatos.agregar(ticket) == true)
+                    {
+
+                        Session.Add("exito", "Enhorabuena Tu Compra se realizo con exito! ");
+                        Response.Redirect("ComprasRealizadas.aspx", false);
+                    }
+
+                }
+
             }
 
         }
--- datos/ArticuloDatos.cs
+++ datos/ArticuloDatos.cs
@@ -271,5 +271,25 @@ namespace datos
         }
 
 
+        public bool ActualizarStock(int id, int nuevostock)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.SetearConsulta("update Articulo set stock = @stock  where id = @id");
+                datos.setearParametro("@id", id);
+                datos.setearParametro("@stock",nuevostock);
+                datos.EjecutarAccion();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+
+        }
+
+
     }
 }
